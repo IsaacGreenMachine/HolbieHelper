a3c782ba476bc6a6bc181a6783a1462168ac4c03
Isaac Green <zuderg@gmail.com>
2022-01-21 12:45:58 -0600

    gets data from intranet

--- intranetTest.js
+++ intranetTest.js
@@ -1,10 +1,18 @@
 #!/usr/bin/node
+if (process.argv.length < 4){
+    console.log("Usage: node intranetTest.js HolbertonEmail HolbertonPassword apiKey(found at: https://intranet.hbtn.io/dashboards/my_tools)")
+}
+else{
 const request = require('request');
-request('https://intranet.hbtn.io', function (error, response, body) {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  console.log(response)
-  console.log(body)
-});
+request.post({
+    headers: {'content-type' : 'application/json'},
+    url:     'https://intranet.hbtn.io/users/auth_token.json',
+    form:    {email: process.argv[2], api_key: process.argv[4], password: process.argv[3], scope: "checker"}
+  }, function(error, response, body){
+    if (error) {
+        console.log(error);
+        return;
+      }
+    console.log(body);
+  });
+}
