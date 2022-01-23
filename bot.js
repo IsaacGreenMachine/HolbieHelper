0d033cb6ece02d6400319ff121ccc98d5d8b180d
jofurdz <jofurdz@gmail.com>
2022-01-23 14:36:21 -0600

    added 3 mp3s

--- bot.js
+++ bot.js
@@ -5,11 +5,15 @@ const commands = require('./commands');
 const utils = require('./utils');
 
 client.on('ready', () => {
-  
+  let userCount = 0
+  client.guilds.cache.forEach(guild => (userCount += guild.memberCount))
 })
 
 client.on('message', message => {
+  //checks that message was meant for bot
   if (!message.content.startsWith(config.prefix) || message.author.bot) return
+  //exits if message isn't in a server
+  if (!message,guild) return
 
   //arg setup
   const args = message.content.slice(config.prefix.length).split(' ')
@@ -25,4 +29,6 @@ client.on('message', message => {
 
 client.on('disconnect', (msg, code) => {
   if (code === 0) return console.error(msg)
-});
+})
+
+client.login(config.token)
